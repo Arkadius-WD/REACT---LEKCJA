8029097a2f96499ab57c301b895bdf119bdd230f
Arcadius-WD <arkadiusz.kamyczek@gmail.com>
2024-01-09 13:58:31 +0100

    routing

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import NotesList from "./components/notes-list/NotesList";
 
 const router = createBrowserRouter([
 	{
-		element: <App />,
 		path: "/",
+		element: <App />,
+	},
+	{
+		path: "/notes/:folderId",
+		element: <NotesList />,
 	},
 ]);
 
